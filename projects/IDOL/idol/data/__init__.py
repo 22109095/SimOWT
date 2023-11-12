b23533fdcd3879e0e613cf5f6919d349a9a46acb
22109095 <103307584+22109095@users.noreply.github.com>
2023-11-12 21:19:54 +0800

    Delete projects/IDOL directory

--- projects/IDOL/idol/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dataset_mapper import YTVISDatasetMapper
-from .coco_dataset_mapper import DetrDatasetMapper
-from .coco_clip import COCO_CLIP_DatasetMapper
-from .build import *
-
-from .datasets import *
-from .ytvis_eval import YTVISEvaluator
--- projects/IDOL/idol/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# ------------------------------------------------------------------------
-# IDOL: In Defense of Online Models for Video Instance Segmentation
-# Copyright (c) 2022 ByteDance. All Rights Reserved.
-# ------------------------------------------------------------------------
