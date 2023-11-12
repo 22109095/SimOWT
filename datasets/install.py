129fa45f4f9ef5db1d162bae8adc3099c25aeeb1
22109095 <103307584+22109095@users.noreply.github.com>
2023-11-12 17:38:52 +0800

    Add files via upload

---
+++ datasets/install.py
@@ -0,0 +1,8 @@
+# conda create -n vnext python=3.7 -y
+# export PATH=/usr/local/cuda-11.1/bin:$PATH
+# conda activate vnext
+# pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+# pip install -r requirements.txt
+# pip install -e .
+# pip install shapely==1.7.1
+# pip install git+https://github.com/youtubevos/cocoapi.git#"egg=pycocotools&subdirectory=PythonAPI"
